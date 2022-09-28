af85e18cc6b2c6463d589aecd4bfa3c9ec0ff713
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-09-28 15:39:36 +0600

    create shudent model

--- models/student.model.js
+++ models/student.model.js
@@ -1,53 +1,53 @@
 const { ObjectID } = require("bson");
 const mongoose = require("mongoose");
-
+const validator = require('validator');
 //schema design
 const studentSchema = mongoose.Schema({
+    imageURL:{
+        type:String,
+        validator:[validator.isURL,"Plese provide a valid url"]
+    },
     name: {
       type: String,
       required: [true, "Please provide a name for thid product"],
       trim: true,
       lowercase:true,
-      unique: [true, "Name must be Unique"],
       minLength: [3, "Name mast be 3 characters"],
       maxLength: [100, "Name is too larges"],
     },
-    /* description: {
-      type: String,
-      required: true
+
+    rollNo:{
+         type:Number,
+         required:true,
+         unique:true,
+         minLength: [6, "Name mast be 3 characters"],
     },
-    unit: {
-      type: String,
-      required: true,
-      enum: {
-        values: ["kg", "liter", "pcs","bag"],
-        massage: "Unit value can't be {VALUE},must be kg/liter/pcs",
-      }
+    admitionDate:{
+        type:String,
+        required:true,
     },
-    imageUrl:[{
-      type:String,
-      required: true,
-      validate:{
-        validator:(value)=>{
-          if(!Array.isArray(value)){
-            return false;
-          }
-          let isValid=true;
-          value.forEach(url =>{
-            if(!validator.isURL(url)){
-              isValid.false;
-            }
-          });
-          return isValid;
-        },
-        massage: "Plese provide valid image url"
-      }
-    }],
-    category:{
-      type:String,
-      require:true,
-    } */
-    
+    parentName:{
+        type:String,
+        required:true,
+    },
+    number:{
+        type:String,
+        required:true,
+    },
+    class:{
+        type:Number,
+        required:true,
+        minLength: [0, "class can not Nagative"],
+        maxLength: [10, "class is not more then 10"],
+    },
+    grade: {
+        type: String,
+        require: true,
+        enum: {
+            values: ['A', 'B', 'C','D','F'],
+            massage: 'status can not be {VALUE}'
+        }
+    },
   }, {
     timestamps: true
   })
